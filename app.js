2837ccfdeca8d0113a5d2559c997a0d76625470c
20134315 AURELIA ALEJANDRA PEREZ GOMEZ <20134315@TELEMATICA.UCOL.MX>
2019-02-20 08:25:54 -0600

    pract1

---
+++ app.js
@@ -0,0 +1,30 @@
+//function statement 
+/*function greet(){
+    console.log('hi');
+}*/
+
+//refactor
+ let greet=()=>{
+    console.log('hi');
+}
+greet();
+
+//refactor
+function logGreeting(fn){
+    fn();
+}
+logGreeting(greet);
+
+//function expresion
+let greetMe = function(){//funcion anonima
+    console.log('Hola Ale');
+}
+greetMe();
+
+//es first-class
+logGreeting(greetMe);
+
+//use a function expression on the fly (directa)
+logGreeting(function(){
+    console.log('Hello');
+});
