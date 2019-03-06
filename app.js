fbd660d1e00f29c5637519e0875a0c805560da37
AureAle <32805589+AureAle@users.noreply.github.com>
2019-03-06 08:38:38 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
-//function statement 
-/*function greet(){
-    console.log('hi');
-}*/
-
-//refactor
- let greet=()=>{
-    console.log('hi');
+//object properties and metods
+var obj = {
+    greet: 'Hello'
 }
-greet();
-//functions are first-class
-/*function logGreeting(fn){
-    fn();
-}
-logGreeting(greet);*/
-//refactor
-let logGreeting=(fn)=>{
-    fn();
-}
-logGreeting(greet);
+console.log(obj.greet);
+console.log(obj['greet']);//accediento a la propiedad de un objeto mediante su clave
+var prop = 'greet';
+console.log(obj[prop]);
 
-//function expresion
-let greetMe = function(){//funcion anonima
-    console.log('Hola Ale');
-}
-greetMe();
+//functions and arrays
 
-//es first-class
-logGreeting(greetMe);
+var arr=[];
 
-//use a function expression on the fly (directa)
-logGreeting(function(){
-    console.log('Hello');
+arr.push(function(){
+    console.log('Jello1');
 });
+arr.push(function(){
+    console.log('Jello2');
+});
+arr.push(function(){
+    console.log('Jello3');
+});
+arr.forEach(function(item){
+  item();
+});
+
+//emitter
+var Emitter = require('events');//node tiene el evento ya definido 'events'
+var eventConfig = require('./config').events;
+
+var emtr = new Emitter();
+
+emtr.on(eventConfig.GREET, function(){//mapeo en el event Greet = 'greet' para evitar errores al escribir
+    console.log('alguien dijo hola');
+});
+
+emtr.on(eventConfig.GREET, function(){
+    console.log('a greeting occurred');//se le agrega ese comportamiento al evento al momento
+});
+
+console.log('Hello!');
+emtr.emit(eventConfig.GREET);
+
---
+++ config.js
@@ -0,0 +1,9 @@
+//mapear constantes dentro de un módulo para evitar errores , Buena Práctica
+module.exports = {
+    events: {
+        GREET:  'greet',
+        FILESAVED: 'saved',
+        
+
+    }
+}
