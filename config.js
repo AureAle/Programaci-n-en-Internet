25e726d14adf1eec646328e0f75bad62c07d0724
AureAle✨ <32805589+AureAle@users.noreply.github.com>
2019-03-20 07:59:04 -0600

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-//mapear constantes dentro de un módulo para evitar errores , Buena Práctica
-module.exports = {
-    events: {
-        GREET:  'greet',
-        FILESAVED: 'saved',
-        
-
-    }
-}
